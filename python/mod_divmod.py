55a0bb842f2a887c25bd8ba617961bd6632e642d
Duy Huynh <kdhuynh@email.arizona.edu>
2015-10-09 01:07:37 -0700

    added mod_divmod.py

---
+++ python/mod_divmod.py
@@ -0,0 +1,14 @@
+
+def main():
+
+    num1 = int(raw_input())
+    num2 = int(raw_input())
+
+    print(num1 // num2)
+    print(num1 % num2)
+    print( divmod(num1, num2))
+
+    return None
+
+if __name__ == '__main__':
+    main()
