72d3cb607af74f5d9601f1357df22b23070a7c4a
Duy Huynh <kdhuynh@email.arizona.edu>
2015-10-09 01:04:24 -0700

    added division.py

--- python/division.py
+++ python/division.py
@@ -1,12 +1,15 @@
 
+from __future__ import division
+
+
 def main():
-    
+
     num1 = int(raw_input())
     num2 = int(raw_input())
 
-    print(num1 + num2)
-    print(num1 - num2)
-    print(num1 * num2)
+    print(num1//num2)
+    print(num1/num2)
+
 
     return None
 
