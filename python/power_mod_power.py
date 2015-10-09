fd819aa444a3fcbe6e78af18e1b267c32c9bcaf2
Duy Huynh <kdhuynh@email.arizona.edu>
2015-10-09 01:11:25 -0700

    added power_mod_power.py

---
+++ python/power_mod_power.py
@@ -0,0 +1,14 @@
+
+def main():
+
+    num1 = int(raw_input())
+    num2 = int(raw_input())
+    num3 = int(raw_input())
+
+    print(pow(num1,num2))
+    print(pow(num1,num2,num3))
+
+    return None
+
+if __name__ == '__main__':
+    main()
