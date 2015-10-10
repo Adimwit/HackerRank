998520ecad6b01bb3aac698ae660b458fbbf1ea4
Duy Huynh <kdhuynh@email.arizona.edu>
2015-10-09 22:49:01 -0700

    added whats your name solution

---
+++ python/whats_your_name.py
@@ -0,0 +1,12 @@
+
+def main():
+
+    firstName = str(raw_input())
+    lastName = str(raw_input())
+
+    print("Hello " + firstName + " " + lastName + "! You just delved into python.")
+
+    return None
+
+if __name__ == "__main__":
+    main()
