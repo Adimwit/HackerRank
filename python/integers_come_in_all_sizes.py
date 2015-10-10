668233ecd0e6265d515388c408ba74e7afc6481f
Duy Huynh <kdhuynh@email.arizona.edu>
2015-10-09 22:25:31 -0700

    added integers_come_in_all_sizes solution

---
+++ python/integers_come_in_all_sizes.py
@@ -0,0 +1,15 @@
+
+def main():
+
+
+    num1 = int(raw_input())
+    num2 = int(raw_input())
+    num3 = int(raw_input())
+    num4 = int(raw_input())
+    print(num1**num2 + num3**num4)
+
+
+    return None
+
+if __name__ == "__main__":
+    main()
